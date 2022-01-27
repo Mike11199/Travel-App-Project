0cdbbc860d355a58865fc834689e8c93a1ea2614
Michael Iwanek <91037796+Mike11199@users.noreply.github.com>
2022-01-27 11:08:26 -0800

    Starting React hooks on map JSX file per tutorial.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+    // starting the hooks here.  useMediaQuery pass in a string so that isMobile variable will be false if the device is larger than 600 pixels.
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0};
+
         return (
-            <h1>Map</h1>
+            <div className={classes.mapContainer}>
+                
+                {/* This component is VERY IMPORTANT.  We will pass a few props into it.  Go to console.cloud.google.com to get API key */}
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: '' }}
+                    defaultCenter={coordinates}
+                    center={coordinates}
+                    defaultZoom={14}
+                    margin={[50,50,50,50]}
+                    options={}
+                    onchange={}
+                    onChildClick={''}
+                >
+
+                </GoogleMapReact>
+
+            </div>>
         );
 }
 
