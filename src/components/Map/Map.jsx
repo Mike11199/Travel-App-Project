7bc54d41f4c5258f325260916906cc775d7c13a1
Michael <miwanek11199@gmail.com>
2022-01-27 12:31:59 -0800

    First build working so far with google maps API.  The problem was with the map styles.js file.  I downloaded this from the tutorials github and corrected it so that CSS is now good

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -24,7 +24,7 @@ const Map = () => {
                     defaultZoom={14}
                     margin={[50,50,50,50]}
                     options={''}
-                    onchange={''}
+                    onChange={''}
                     onChildClick={''}
                 >
 
